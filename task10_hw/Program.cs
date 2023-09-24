dacf101004c39748b15cccc015024f880aeef801
romanstukalkin <stukalkinrv@yandex.ru>
2023-09-24 17:45:16 +0400

    task10 HW

---
+++ task10_hw/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Введите 3х значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999) Console.WriteLine("Введено НЕ 3х значное число");
+else 
+{
+    Console.WriteLine($"{number} -> {(number % 100) / 10}");
+}
