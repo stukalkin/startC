7ce526a061d0fd41eb4ed40abc4e01f423148ed3
romanstukalkin <stukalkinrv@yandex.ru>
2023-09-30 09:56:41 +0400

    task25_hw

---
+++ task25_hw/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+void Cicl(int a, int b)
+{
+    int proizv = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        proizv = proizv * a;
+    }
+    Console.WriteLine(proizv);
+}
+
+Cicl(2, 4);
