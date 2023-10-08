13dd4e76dc36e50a2dcbd1e1ed279ea53811366b
romanstukalkin <stukalkinrv@yandex.ru>
2023-10-08 12:45:47 +0400

    hw41

---
+++ task41_hw/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int[] ArrAdd(int num)
+{
+    int[] arr = new int[num];
+    Console.WriteLine("Пришло время заполнить массив");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i+1}-й элемент массива");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int CountArr(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+void PrintArray(int[] arr)
+{
+    //Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    //Console.Write("]");
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Введите число элементов");
+int colElem = Convert.ToInt32(Console.ReadLine());
+int[] array = ArrAdd(colElem);
+int result = CountArr(array);
+PrintArray(array);
+Console.WriteLine($"Колличество положительных элементов - {result}");
---
+++ task44/Program.cs
@@ -0,0 +1,92 @@
+﻿/*Задача 44: Не используя рекурсию, выведите первые N чисел
+Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8*/
+
+/*int[] FiboArray(int num)
+{
+    int[] fibonacci= new int[num];
+    //fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    for (int i = 2; i < fibonacci.Length; i++)
+    {
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }  
+    return fibonacci;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите число цифр Фибоначчи");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 2) 
+{
+    Console.WriteLine("Введите число более 1");
+    return;
+}
+
+int[] result = FiboArray(number);
+Console.Write($"Если N = {number} -> ");
+PrintArray(result);*/
+
+
+/*Задача 45: Напишите программу, которая будет создавать
+копию заданного массива с помощью поэлементного
+копирования.*/
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    //Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    //Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] copy = new int[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copy[i] = arr[i];
+    }
+    return copy;
+}
+
+int[] firstArray = CreateArrayRndInt(8, 1, 11);
+PrintArray(firstArray);
+int[] second = CopyArray(firstArray);
+second[0] = -1;
+PrintArray(second);
+int[] third = firstArray;
+PrintArray(third);
+firstArray[0] = -2;
+PrintArray(third);
